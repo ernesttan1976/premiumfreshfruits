3bf41fd90dd2784fb81f6ce811942e8ded73900e
ernesttan1976 <ernesttan1976@gmail.com>
2023-07-17 23:01:30 +0800

    togglereload with timeout

--- app/page.js
+++ app/page.js
@@ -73,7 +73,10 @@ export default function Home({children}) {
   }
 
   function handleReload(){
-    setToggleReload(!toggleReload)
+    setTimeout(() => {
+      setToggleReload(!toggleReload)
+    }, 500);
+    
   }
 
   const handlePlaceOrder = async (ev) => {
